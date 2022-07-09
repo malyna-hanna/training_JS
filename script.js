e3c747547a16beb009cc3850ec0c29484a573b88
malyna-hanna <malina10087@gmail.com>
2022-07-09 16:58:59 +0300

    training

---
+++ script.js
@@ -0,0 +1,30 @@
+// const result = confirm('Are you stay here?');
+// console.log(result);
+
+// const answer = +prompt('How old are you?', '18');
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = +prompt('How old are you?', '');
+// document.write(answers);
+// console.log(typeof(answers), answers);
+
+// *Интерполяция*
+
+// const category = 'toys';
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = prompt('What is your name');
+// console.log(`Hello, ${user}!`);
+// alert(`Hello, ${user}!`);
+
+// *Инкремент/Декремент*
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr, decr);
